ec089ac24a5c69b26bba98d303782732d38caa65
Sachi Parekh <sachi.p@somaiya.edu>
2026-01-27 18:16:46 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,24 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+/* ================================
+   Google Analytics (Runtime Inject)
+   ================================ */
+const gaScript1 = document.createElement("script");
+gaScript1.async = true;
+gaScript1.src = "https://www.googletagmanager.com/gtag/js?id=G-WYKW80BMKN";
+document.head.appendChild(gaScript1);
+
+const gaScript2 = document.createElement("script");
+gaScript2.innerHTML = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-WYKW80BMKN');
+`;
+document.head.appendChild(gaScript2);
+/* ================================ */
+
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter basename="/data-encoders-hub-19">
     <App />
